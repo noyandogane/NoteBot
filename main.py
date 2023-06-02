2df2ad1b62e81171a472428d493a64749f9ea5b9
noyandogane <ltmpinfo@gmail.com>
2023-06-02 11:36:14 +0300

    Refactor note keeping bot commands and add reminder functionality

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import discord
 from discord.ext import commands
 import datetime
+import asyncio
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -11,71 +12,127 @@ notes = {}  # format: {user_id: {category: [note1, note2,...]}}
 
 @bot.command()
 async def help(ctx):
-    await ctx.send('''
-    **!add <category> <note>** - Add a note to a category
-    **!view** - View all your notes
-    **!view_category <category>** - View notes from a specific category
-    **!delete <category> <number>** - Delete a note from a category
-    **!edit <category> <number> <new_note>** - Edit a note from a category
-    **!search <keyword>** - Search for a keyword in all your notes
-    '''
-    )
+    embed = discord.Embed(title='Note Keeper Bot Help', description='Use the following commands to manage your notes:', color=discord.Color.blue())
+    embed.add_field(name='!add [category] <note>', value='Add a note to a category. If no category is specified, the note will be added without a category.', inline=False)
+    embed.add_field(name='!view', value='View all your notes', inline=False)
+    embed.add_field(name='!view_category <category>', value='View notes from a specific category', inline=False)
+    embed.add_field(name='!delete <category> <number>', value='Delete a note from a category', inline=False)
+    embed.add_field(name='!edit <category> <number> <new_note>', value='Edit a note from a category', inline=False)
+    embed.add_field(name='!search <keyword>', value='Search for a keyword in all your notes', inline=False)
+    embed.add_field(name='!remind <category> <number> <duration>', value='Set a reminder for a note', inline=False)
+    await ctx.send(embed=embed)
 
 @bot.command()
-async def add(ctx, category: str, *, note: str):
+async def add(ctx, category: str = None, *, note: str):
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
     note = f"{timestamp}: {note}"
-    if ctx.message.author.id not in notes:
-        notes[ctx.message.author.id] = {}
-    if category not in notes[ctx.message.author.id]:
-        notes[ctx.message.author.id][category] = []
-    notes[ctx.message.author.id][category].append(note)
-    await ctx.send('Note added to the category!')
+    user_notes = notes.setdefault(ctx.message.author.id, {})
+    if category:
+        user_notes.setdefault(category, []).append(note)
+        await ctx.send('Note added to the category!')
+    else:
+        user_notes.setdefault('Uncategorized', []).append(note)
+        await ctx.send('Note added without a category!')
 
 @bot.command()
 async def view(ctx):
-    if ctx.message.author.id in notes and len(notes[ctx.message.author.id]) > 0:
-        for category, notes_list in notes[ctx.message.author.id].items():
-            await ctx.send(f'{category}:\n' + '\n'.join(notes_list))
+    user_notes = notes.get(ctx.message.author.id)
+    if user_notes:
+        embed = discord.Embed(title='Your Notes', color=discord.Color.green())
+        for category, notes_list in user_notes.items():
+            formatted_notes = '\n'.join([f'Note {i}: {note}' for i, note in enumerate(notes_list, 1)])
+            embed.add_field(name=f'Category: {category}', value=formatted_notes, inline=False)
+        await ctx.send(embed=embed)
     else:
         await ctx.send('You have no notes.')
 
 @bot.command()
 async def view_category(ctx, category: str):
-    if ctx.message.author.id in notes and category in notes[ctx.message.author.id]:
-        await ctx.send('\n'.join(notes[ctx.message.author.id][category]))
+    user_notes = notes.get(ctx.message.author.id)
+    if user_notes and category in user_notes:
+        notes_list = user_notes[category]
+        formatted_notes = '\n'.join([f'Note {i}: {note}' for i, note in enumerate(notes_list, 1)])
+        embed = discord.Embed(title=f'Notes in Category: {category}', description=formatted_notes, color=discord.Color.green())
+        await ctx.send(embed=embed)
     else:
         await ctx.send('No notes in this category.')
 
 @bot.command()
 async def delete(ctx, category: str, number: int):
-    if ctx.message.author.id in notes and category in notes[ctx.message.author.id] and len(notes[ctx.message.author.id][category]) >= number:
-        deleted_note = notes[ctx.message.author.id][category].pop(number - 1)
-        await ctx.send(f'Deleted note: {deleted_note}')
+    user_notes = notes.get(ctx.message.author.id)
+    if user_notes and category in user_notes:
+        notes_list = user_notes[category]
+        if 1 <= number <= len(notes_list):
+            deleted_note = notes_list.pop(number - 1)
+            await ctx.send(f'Deleted note: {deleted_note}')
+        else:
+            await ctx.send('Invalid note number.')
     else:
-        await ctx.send('Could not find note to delete. Make sure you have entered the correct note number.')
+        await ctx.send('Could not find notes to delete.')
 
 @bot.command()
 async def edit(ctx, category: str, number: int, *, new_note: str):
-    if ctx.message.author.id in notes and category in notes[ctx.message.author.id] and len(notes[ctx.message.author.id][category]) >= number:
-        notes[ctx.message.author.id][category][number - 1] = new_note
-        await ctx.send('Note updated!')
+    user_notes = notes.get(ctx.message.author.id)
+    if user_notes and category in user_notes:
+        notes_list = user_notes[category]
+        if 1 <= number <= len(notes_list):
+            notes_list[number - 1] = new_note
+            await ctx.send('Note updated!')
+        else:
+            await ctx.send('Invalid note number.')
     else:
-        await ctx.send('Could not find note to edit. Make sure you have entered the correct note number.')
+        await ctx.send('Could not find notes to edit.')
 
 @bot.command()
 async def search(ctx, keyword: str):
-    if ctx.message.author.id in notes and len(notes[ctx.message.author.id]) > 0:
-        found = False
-        for category, notes_list in notes[ctx.message.author.id].items():
-            matches = [note for note in notes_list if keyword.lower() in note.lower()]
+    user_notes = notes.get(ctx.message.author.id)
+    if user_notes:
+        found_notes = []
+        for category, notes_list in user_notes.items():
+            matches = [f'Note {i}: {note}' for i, note in enumerate(notes_list, 1) if keyword.lower() in note.lower()]
             if matches:
-                found = True
-                await ctx.send(f'{category}:\n' + '\n'.join(matches))
-        if not found:
+                found_notes.extend(matches)
+        if found_notes:
+            formatted_notes = '\n'.join(found_notes)
+            embed = discord.Embed(title=f'Matching Notes for Keyword: {keyword}', description=formatted_notes, color=discord.Color.orange())
+            await ctx.send(embed=embed)
+        else:
             await ctx.send('No matching notes found.')
     else:
         await ctx.send('You have no notes.')
-    
+
+@bot.command()
+async def remind(ctx, category: str, number: int, duration: str):
+    user_notes = notes.get(ctx.message.author.id)
+    if user_notes and category in user_notes:
+        notes_list = user_notes[category]
+        if 1 <= number <= len(notes_list):
+            note = notes_list[number - 1]
+            reminder_time = get_duration(duration)
+            if reminder_time:
+                await asyncio.sleep(reminder_time)
+                embed = discord.Embed(title='Note Reminder', description=f'Reminder for note in category: {category}', color=discord.Color.gold())
+                embed.add_field(name='Note', value=note, inline=False)
+                await ctx.send(embed=embed)
+            else:
+                await ctx.send('Invalid reminder duration.')
+        else:
+            await ctx.send('Invalid note number.')
+    else:
+        await ctx.send('Could not find notes to set a reminder.')
+
+def get_duration(duration):
+    units = {
+        's': 1,
+        'm': 60,
+        'h': 3600,
+        'd': 86400
+    }
+    try:
+        value = int(duration[:-1])
+        unit = duration[-1]
+        return value * units.get(unit)
+    except (ValueError, KeyError):
+        return None
+
 bot.run(os.environ["DISCORD_TOKEN"])
-               
