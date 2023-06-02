78b3bd7855ed9dded098f06f7d75cd73cdb86121
noyandogane <ltmpinfo@gmail.com>
2023-06-02 11:02:45 +0300

    Improve the Help Command and Remove Default Help Command
    This commit improves the `!help` command by using an embed instead of a plain text. Now, it displays help commands in a more user-friendly and informative way with detailed descriptions. Also, the default help command has been removed and replaced with a `None` value.

--- main.py
+++ main.py
@@ -5,17 +5,19 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='!', intents=intents )
+bot = commands.Bot(command_prefix='!', intents=intents, help_command=None )
 notes = {}
 
 @bot.command()
 async def help(ctx):
-    await ctx.send('''
-    **!add** - Add a note
-    **!view** - View your notes
-    **!delete** - Delete a note
-    '''
-    )
+    embed = discord.Embed(title="Help", description="List of commands are:", color=discord.Color.blue())
+
+    embed.add_field(name="!add [note]", value="Add a note", inline=False)
+    embed.add_field(name="!view", value="View your notes", inline=False)
+    embed.add_field(name="!delete [number]", value="Delete a note", inline=False)
+
+    await ctx.send(embed=embed)
+
 @bot.command()
 async def add(ctx, *, note: str):
     if ctx.message.author.id not in notes:
