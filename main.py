c74afdda5e8569d4d30217218b858590b59a89e6
noyandogane <ltmpinfo@gmail.com>
2023-06-02 10:39:52 +0300

    notebot initial commit

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
-# This example requires the 'message_content' privileged intents
-
-import os
 import discord
 from discord.ext import commands
 
+bot = commands.Bot(command_prefix='!')
 
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
-
-
-@bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user}")
+notes = {}
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def add(ctx, *, note: str):
+    if ctx.message.author.id not in notes:
+        notes[ctx.message.author.id] = []
+    notes[ctx.message.author.id].append(note)
+    await ctx.send('Note added!')
 
 @bot.command()
-async def hello(ctx):
-    await ctx.send("Choo choo! 🚅")
+async def view(ctx):
+    if ctx.message.author.id in notes and len(notes[ctx.message.author.id]) > 0:
+        await ctx.send('\n'.join(notes[ctx.message.author.id]))
+    else:
+        await ctx.send('You have no notes.')
+
+@bot.command()
+async def delete(ctx, number: int):
+    if ctx.message.author.id in notes and len(notes[ctx.message.author.id]) >= number:
+        deleted_note = notes[ctx.message.author.id].pop(number - 1)
+        await ctx.send(f'Deleted note: {deleted_note}')
+    else:
+        await ctx.send('Could not find note to delete. Make sure you have entered the correct note number.')
+
 
 
 bot.run(os.environ["DISCORD_TOKEN"])
