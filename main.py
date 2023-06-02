671aa881b68dea6e3dccf1b9cdb1fa9d4a14e45a
noyandogane <ltmpinfo@gmail.com>
2023-06-02 10:56:49 +0300

    a

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='!', intents=intents help_command=None)
+bot = commands.Bot(command_prefix='!', intents=intents )
 notes = {}
 
 @bot.command()
